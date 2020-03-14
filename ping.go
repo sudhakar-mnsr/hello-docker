559faa383f1ab00a5638e8a1ea0bbfa65293d26a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-14 23:32:41 +0530

    examples

--- ping.go
+++ ping.go
@@ -30,3 +30,21 @@ func main() {
 
    conn, err := net.DialIP("ip4:icmp", localAddr, remoteAddr)
    checkError(err)
+
+   var msg [512]byte
+   msg[0] = 8  // echo
+   msg[1] = 0  // code 0
+   msg[2] = 0  // checksum, fix later
+   msg[3] = 0  // checksum, fix later
+   msg[4] = 0  // identifier[0]
+   msg[5] = 13 // identifier[1] (arbitrary)
+   msg[6] = 0  // sequence[0]
+   msg[7] = 37 // sequence[1] (arbitrary)
+   len := 8
+
+   // now fix checksum bytes
+   check := checkSum(msg[0:len])
+   msg[2] = byte(check >> 8)
+   msg[3] = byte(check & 255)
+
+   // send the message
