29911e1c7e46fc2e262ffa9a01fea98d00775b1d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-17 20:16:38 +0530

    examples

--- echos2.go
+++ echos2.go
@@ -25,3 +25,28 @@ func main() {
       os.Exit(1)
    }
 
+   // announce service using ListenUnix
+   // which creates a UnixListener listener
+   l, err := net.ListenUnix("unix", laddr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   defer l.Close()
+   fmt.Println("listening at (unix)", laddr.String())
+
+   // req/response loop
+   for {
+      // use UnixListener to block and wait for UDS
+      // connection request using AcceptUnix which then
+      // creates a UnixConn
+      conn, err := l.AcceptUnix()
+      if err != nil {
+         fmt.Println("failed to accept conn:", err)
+         conn.Close()
+         continue
+      }
+      fmt.Println("connected to : ", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
+}
