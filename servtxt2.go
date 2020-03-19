33e16c8194b51fe77aff6b7c87db1a41a39c96b3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-19 14:17:37 +0530

    examples

--- servtxt2.go
+++ servtxt2.go
@@ -80,3 +80,43 @@ func main() {
       go handleConnection(conn)
    }
 }
+
+func handleConnection(conn net.Conn) {
+   defer func() {
+      if err := conn.Close(); err != nil {
+         log.Println("error closing connection:", err)
+      }
+   }()
+   if _, err := fmt.Fprint(conn, "Connected...\nUsage: GET <currency, country, or code>\n"); err != nil {
+      log.Println("error writing:", err)
+      return
+   }
+
+   // buffered reader to stream data using 4 byte chunks until \n
+   // The chunks are kept small to demonstrate streaming using io.Reader.
+   reader := bufio.NewReaderSize(conn, 4)
+
+   // command-loop
+   for {
+      cmdLIne, err := reader.ReadString('\n')
+      if err != nil {
+         if err != io.EOF {
+            log.Println("connection read error:", err)
+            return
+         }
+      }
+      reader.Reset(conn) //cleans buffer
+
+      cmd, param := parseCommand(cmdLine)
+      if cmd == "" {
+         if _, err := fmt.Fprint(conn, "Invalid command\n"); err != nil {
+            log.Println("failed to write:", err)
+            return
+         }
+         continue
+      }
+
+      // execute command
+      switch strings.ToUpper(cmd) {
+      case "GET":
+         result := curr.Find(currencies, param)
