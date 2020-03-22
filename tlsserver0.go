4ba0f0aeac33d03b74ecde5dee68c463f5b50a52
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-22 23:30:09 +0530

    added back

--- tlsserver0.go
+++ tlsserver0.go
@@ -46,3 +46,20 @@ var (
 // options:
 //   -e host endpoint, default ":4443"
 //   -n network protocol [tcp,unix], default "tcp"
+
+func main() {
+	// setup flags
+	var addr, network, cert, key string
+	flag.StringVar(&addr, "e", ":4443", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.StringVar(&cert, "cert", "../certs/localhost-cert.pem", "public cert")
+	flag.StringVar(&key, "key", "../certs/localhost-key.pem", "private key")
+	flag.Parse()
+
+	// validate supported network protocols
+	switch network {
+	case "tcp", "tcp4", "tcp6", "unix":
+	default:
+		fmt.Println("unsupported network protocol")
+		os.Exit(1)
+	}
