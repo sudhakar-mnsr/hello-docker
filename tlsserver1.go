a1882c4811d31bc9242daef68388d34970f6e9d1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-22 23:40:49 +0530

    added back

--- tlsserver1.go
+++ tlsserver1.go
@@ -30,3 +30,24 @@ const prompt = "currency"
 //  - n network protocol name [tcp,unix], default tcp
 //
 // Once started a prompt is provided to interact with service.
+
+func main() {
+	// setup flags
+	var addr, network, cert, key, ca string
+	flag.StringVar(&addr, "e", "localhost:4443", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.StringVar(&cert, "cert", "../certs/client-cert.pem", "public cert")
+	flag.StringVar(&key, "key", "../certs/client-key.pem", "private key")
+	flag.StringVar(&ca, "ca", "../certs/ca-cert.pem", "root CA certificate")
+	flag.Parse()
+
+	cer, err := tls.LoadX509KeyPair(cert, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load our CA certificate
+	caCert, err := ioutil.ReadFile(ca)
+	if err != nil {
+		log.Fatal("failed to read CA cert", err)
+	}
