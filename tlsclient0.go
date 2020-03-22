4ccf08e57e4c10c9bf62a024847903790ba05cc1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-22 23:26:08 +0530

    added back

--- tlsclient0.go
+++ tlsclient0.go
@@ -47,3 +47,19 @@ func main() {
 
 	certPool := x509.NewCertPool()
 	certPool.AppendCertsFromPEM(caCert)
+
+	// TLS configuration
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: false,
+		RootCAs:            certPool,
+	}
+
+	// create a tls.Conn to connect to server
+	conn, err := tls.Dial(network, addr, tlsConf)
+	if err != nil {
+		log.Fatal("failed to create socket:", err)
+	}
+	defer conn.Close()
+	fmt.Println("connected to currency service: ", addr)
+
+	var param string
